d7c7c8c138b0940fb0e0df6e1cf6a3a870962440
goliatone <burgosemi@gmail.com>
2017-05-30 10:40:12 -0400

    Silence brew
    Opt out of brew analytics...

--- modules/brew.sh
+++ modules/brew.sh
@@ -8,7 +8,17 @@ else
   echo "Command line tools not installed!" && exit 1
 fi
 
-# 2. Tap boneyard for Brewfile support
+# 2. Disable analytics
+# Create .bash_profile if it doesn't exist
+if [[ ! -f ~/.bash_profile ]]; then touch ~/.bash_profile; fi
+echo 'export HOMEBREW_ANALYTICS_DEBUG=1' >> ~/.bash_profile
+# Also, just tell brew to not care about what we do...
+if which brew > /dev/null; then
+  brew analytics off
+fi
+
+
+# 3. Tap boneyard for Brewfile support
 if which brew > /dev/null; then
   brew tap Homebrew/bundle
 else
