e1785d2a61a6e18c74495ca25b9a9dd4afeb306c
goliatone <burgosemi@gmail.com>
2017-12-09 17:29:50 -0500

    update python

--- modules/python.sh
+++ modules/python.sh
@@ -3,7 +3,11 @@
 echo "You should run brew info pyenv and read the caveats about putting the eval() in your profile"
 eval "$(pyenv init -)"
 
-pyenv update
-pyenv install 2.7.10
-pyenv shell 2.7.10
-pyenv global 2.7.10
+#Apple has deprecated use of OpenSSL in favor of its own TLS and crypto libraries openssl is
+#not symlinked to /usr/local. Include flags
+CFLAGS="-I$(brew --prefix openssl)/include" \
+LDFLAGS="-L$(brew --prefix openssl)/lib" \
+
+pyenv install 2.7.12
+pyenv global 2.7.12
+pyenv install 3.6.0
